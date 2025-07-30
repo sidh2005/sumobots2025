f8e873af1f284b3779e077839f55085b9c751fe9
Sidhida Behera <z5588034@ad.unsw.edu.au>
2025-07-31 09:51:15 +1000

    add: Added sumobotSlowTurn.ino as a testing file to test slow turning

---
+++ sumobot/sumobotSlowTurn.ino
@@ -0,0 +1,223 @@
+#include <Arduino.h>
+#include <Servo.h>
+Servo rampServo;
+
+// pin_init_left
+const int IN1_FL = 2;
+const int IN2_FL = 3;
+const int PWM_FL = 4;
+
+const int IN1_BL = 5;
+const int IN2_BL = 6;
+const int PWM_BL = 7;
+
+// pin_init_right
+const int IN1_FR = 8;
+const int IN2_FR = 9;
+const int PWM_FR = 10;
+
+const int IN1_BR = 11;
+const int IN2_BR = 12;
+const int PWM_BR = 13; 
+
+// ultrasonic_init
+const int trigPins[3] = {51, 39, 31};  
+const int echoPins[3] = {53, 41, 33};
+const int numSensors = 3; 
+const int detectionThreshold = 30; // cm
+
+// motor_init
+int motorSpeed = 255; // 0–255 //keep at !max! cause backEMF stuff 
+
+unsigned long lastActionTime = 0;
+unsigned long cooldown = 500;
+
+void setup() {
+  delay(5000);
+  rampServo.attach(48);
+  rampServo.write(90);
+  delay(400); // quick ramp drop
+  rampServo.write(0);
+  delay(200); // quick return
+
+  Serial.begin(9600);
+
+  // Set motor pins as output
+  int motorPins[] = {
+    IN1_FL, IN2_FL, PWM_FL,
+    IN1_BL, IN2_BL, PWM_BL,
+    IN1_FR, IN2_FR, PWM_FR,
+    IN1_BR, IN2_BR, PWM_BR
+  };
+
+  for (int i = 0; i < 12; i++) {
+    pinMode(motorPins[i], OUTPUT);
+    digitalWrite(motorPins[i], LOW);
+  }
+
+  // Ultrasonic init thingo 
+  for (int i = 0; i < numSensors; i++) {
+    pinMode(trigPins[i], OUTPUT);
+    pinMode(echoPins[i], INPUT);
+  }
+
+  stopAll();
+}
+
+void loop() {
+  unsigned long now = millis();
+  if (now - lastActionTime < cooldown) return;
+
+  long distances[3];
+  for (int i = 0; i < numSensors; i++) {
+    distances[i] = readUltrasonic(trigPins[i], echoPins[i]);
+  }
+
+  //make middle sensor to the middle turn right or left to face enemy
+  //charge AFTER sensor is aligned 
+  if (distances[1] > 0 && distances[1] < detectionThreshold) {
+    setMotorSpeed(255);
+    moveForward();
+  } else if (distances[0] > 0 && distances[0] < detectionThreshold) {
+    turnUntilCenterSeesOpponent(moveRight);
+  } else if (distances[2] > 0 && distances[2] < detectionThreshold) {
+    turnUntilCenterSeesOpponent(moveLeft);
+  } else { 
+    searchLogic();
+  }
+
+  lastActionTime = millis();
+  delay(100);
+}
+
+void turnUntilCenterSeesOpponent(void (*turnFunc)()) {
+  setMotorSpeed(180); // slow + controlled turn
+  unsigned long start = millis();
+  while (true) {
+    turnFunc();
+    long centerDist = readUltrasonic(trigPins[1], echoPins[1]);
+    if (centerDist > 0 && centerDist < detectionThreshold) break;
+    if (millis() - start > 2000) {
+      stopAll();
+      delay(100);
+      searchLogic();
+      return;
+    }
+    delay(50);
+  }
+  stopAll();
+  delay(100);
+}
+
+void moveForward() {
+  // front_left
+  digitalWrite(IN1_FL, HIGH);
+  digitalWrite(IN2_FL, LOW);
+  analogWrite(PWM_FL, motorSpeed);
+
+  // back_left
+  digitalWrite(IN1_BL, HIGH);
+  digitalWrite(IN2_BL, LOW);
+  analogWrite(PWM_BL, motorSpeed);
+
+  // front_right
+  digitalWrite(IN1_FR, HIGH);
+  digitalWrite(IN2_FR, LOW);
+  analogWrite(PWM_FR, motorSpeed);
+
+  // back_right
+  digitalWrite(IN1_BR, HIGH);
+  digitalWrite(IN2_BR, LOW);
+  analogWrite(PWM_BR, motorSpeed);
+}
+
+void moveRight() {
+  // left_ahead
+  digitalWrite(IN1_FL, HIGH);
+  digitalWrite(IN2_FL, LOW);
+  analogWrite(PWM_FL, motorSpeed);
+
+  digitalWrite(IN1_BL, HIGH);
+  digitalWrite(IN2_BL, LOW);
+  analogWrite(PWM_BL, motorSpeed);
+
+  // right_spins_back
+  digitalWrite(IN1_FR, LOW);
+  digitalWrite(IN2_FR, HIGH);
+  analogWrite(PWM_FR, motorSpeed);
+
+  digitalWrite(IN1_BR, LOW);
+  digitalWrite(IN2_BR, HIGH);
+  analogWrite(PWM_BR, motorSpeed);
+}
+
+void moveLeft() {
+  // left_back
+  digitalWrite(IN1_FL, LOW);
+  digitalWrite(IN2_FL, HIGH); 
+  analogWrite(PWM_FL, motorSpeed);
+
+  digitalWrite(IN1_BL, LOW);
+  digitalWrite(IN2_BL, HIGH);
+  analogWrite(PWM_BL, motorSpeed);
+
+  // right_spins_forward
+  digitalWrite(IN1_FR, HIGH);
+  digitalWrite(IN2_FR, LOW);
+  analogWrite(PWM_FR, motorSpeed);
+
+  digitalWrite(IN1_BR, HIGH);
+  digitalWrite(IN2_BR, LOW);
+  analogWrite(PWM_BR, motorSpeed);
+}
+
+void stopAll() {
+  analogWrite(PWM_FL, 0);
+  analogWrite(PWM_BL, 0);
+  analogWrite(PWM_FR, 0);
+  analogWrite(PWM_BR, 0);
+}
+
+// ultrasonic_read
+long readUltrasonic(int trigPin, int echoPin) {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  long duration = pulseIn(echoPin, HIGH, 30000);
+  return duration * 0.034 / 2; 
+}
+
+void setMotorSpeed(int speed) {
+  motorSpeed = speed;
+}
+
+void searchLogic() {
+  setMotorSpeed(190);
+  moveRight();
+  delay(500);
+  stopAll();
+  delay(100);
+  if (readUltrasonic(trigPins[1], echoPins[1]) < detectionThreshold) {
+    setMotorSpeed(255);
+    moveForward();
+    return;
+  }
+
+  moveLeft();
+  delay(1000);
+  stopAll();
+  delay(100);
+  if (readUltrasonic(trigPins[1], echoPins[1]) < detectionThreshold) {
+    setMotorSpeed(255);
+    moveForward();
+    return;
+  }
+
+  moveRight();
+  delay(500);
+  stopAll();
+  delay(100);
+}
